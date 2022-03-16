33c20fa277a30c2336610a78adf891159e016e51
Samrawit Shebeshe Awoke <87376475+SamrawitS@users.noreply.github.com>
2022-03-16 22:00:36 +0300

    02 print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - main block
+ * Return: 0
+ **/
+int main(void)
+{
+      	char x[] = "_putchar";
+
+	      int Y;
+
+      	for (Y = 0; Y < 8; Y++)
+      	{
+		            _putchar(x[k]);
+      	}
+      	_putchar('\n');
+      	return (0);
+}
