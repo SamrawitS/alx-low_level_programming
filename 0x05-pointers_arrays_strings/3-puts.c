d69be9249594af2c1a0f2675a65e17e962f7150f
Samrawit Shebeshe Awoke <87376475+SamrawitS@users.noreply.github.com>
2022-03-22 06:50:16 +0300

    05 put

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * puts - prints a string followed by a new line
- * @str: input
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
-		i++;
 	}
 	_putchar('\n');
 }
