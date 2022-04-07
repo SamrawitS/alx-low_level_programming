b422d42191a3ab06df67e05dd79b81d384173915
Samrawit Shebeshe Awoke <87376475+SamrawitS@users.noreply.github.com>
2022-04-07 21:26:07 +0300

    Create 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - multiply 2 numbers as arguments
+* @num1: postive number
+* @num2: postive number
+* Return: 0 success
+*/
+int main(int num1, num2)
+{
+	unsigned long mul;
+	int num1, num2;
+	if (num1 && num2 <= )
+	{
+	 printf("Error\n");
+	exit(98); 
+	}
+	else
+	{
+	mul = num1 *  num2);
+	printf("%lu\n", mul);
+	}
+return (0);
+}
